3e875b377487446c818124710771df92ac8bdcfd
Osman Başkaya <thyme.exe@gmail.com>
2011-11-12 00:00:47 +0200

    output more informative: timestamp etc.

--- src/test.py
+++ src/test.py
@@ -4,6 +4,7 @@
 from evaluate import Evaluater
 from itertools import product
 import cProfile
+from time import gmtime, strftime
 
 
 def main():
@@ -14,12 +15,14 @@ def main():
     #k_val = [3, 9, 13, 17, 21]
     iterate = product(per, k_val)
     for per, k in iterate:
-        f.write(str([per, k]) + '\t')
+        timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        f.write("New test on: " + timestamp + ' --> ')
         e = Evaluater('u.data', k=k, test_percentage=per, eval_metric='mae')
+        f.write(str([per, k, e.eval_metric, e.sim_method.func_name]) + ' Error: ')
         f.write(str(e.evaluate()) + '\n')
     f.close()
 
 
 if __name__ == '__main__':
-    cProfile.run('main()')
-    #main()
+    #cProfile.run('main()')
+    main()
